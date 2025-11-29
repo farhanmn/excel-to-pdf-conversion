889b69699181038cb4166fe6f80b661be9a8ce8a
Muhammad Farhan N <najibfarhan3@gmail.com>
2025-11-30 05:31:32 +0700

    new template

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,83 +1,84 @@
-import fs from 'fs'
-import path from 'path'
-import xlsx from 'xlsx'
-import puppeteer from 'puppeteer'
-import { StatusCodes as SC } from 'http-status-codes'
+import fs from 'fs';
+import path from 'path';
+import xlsx from 'xlsx';
+import puppeteer from 'puppeteer';
+import { StatusCodes as SC } from 'http-status-codes';
 
 import {
   errorFormat,
-  excelFormat,
+  // excelFormat,
+  excelNewFormat
   // excelFormatPrestasi,
-} from '#helper/format.js'
+} from '#helper/format.js';
 
-import defaultResponse from '#helper/response.js'
-import { getTemplate } from '#helper/templating.js'
+import defaultResponse from '#helper/response.js';
+import { getTemplate } from '#helper/templating.js';
 
 const convertExcel = async (req, res) => {
-  const { file } = req
+  const { file } = req;
   if (!file) {
-    return res.status(400).json({ message: 'No uploaded file' })
+    return res.status(400).json({ message: 'No uploaded file' });
   }
 
-  const outputDir = 'generated-pdfs'
+  const outputDir = 'generated-pdfs';
   if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir)
+    fs.mkdirSync(outputDir);
   }
 
   try {
-    const workbook = xlsx.readFile(file.path)
-    const sheetName = workbook.SheetNames[0]
-    const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName])
+    const workbook = xlsx.readFile(file.path);
+    const sheetName = workbook.SheetNames[0];
+    const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
-    const batchSize = 100 // Jumlah baris per batch
-    let total = 0
+    const batchSize = 100; // Jumlah baris per batch
+    let total = 0;
 
     for (let i = 0; i < sheetData.length; i += batchSize) {
-      const batch = sheetData.slice(i, i + batchSize)
+      const batch = sheetData.slice(i, i + batchSize);
 
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
       for (let [, row] of batch.entries()) {
         // console.log(excelFormat(row))
-        let format = excelFormat(row)
-        const { html } = getTemplate('excel_template', format)
-        await page.setContent(html, { waitUntil: 'load' })
+        let format = excelNewFormat(row);
+        const { html } = getTemplate('excel_template_2025', format);
+        await page.setContent(html, { waitUntil: 'load' });
 
         const pdfPath = path.join(
           outputDir,
-          `Inspirasi - ${format.name} - ${format.id}.pdf`
-        )
+          `${format.name} - ${format.id}.pdf`
+        );
         await page.pdf({
           path: pdfPath,
           printBackground: true, // Menyertakan background warna & gambar dari CSS
           width: '37.8cm',
           height: '53.46cm',
-          preferCSSPageSize: true, // Menggunakan ukuran dari CSS @page jika ada
-        })
+          preferCSSPageSize: true // Menggunakan ukuran dari CSS @page jika ada
+        });
 
-        total++
+        total++;
       }
 
-      await browser.close()
+      await browser.close();
     }
 
-    fs.unlinkSync(file.path) // Hapus file upload setelah diproses
+    fs.unlinkSync(file.path); // Hapus file upload setelah diproses
 
     return res.status(SC.OK).json(
       defaultResponse.renderData({
-        message: 'File uploaded successfully by total ' + total,
+        message: 'File uploaded successfully by total ' + total
       })
-    )
+    );
   } catch (e) {
-    console.log(e)
-    const { stCode, message } = errorFormat(e)
+    console.log(e);
+    const { stCode, message } = errorFormat(e);
     return res.status(stCode).json(
       defaultResponse.renderError({
-        message,
+        message
       })
-    )
+    );
   }
-}
+};
 
-export { convertExcel }
+export { convertExcel };
--- helper/format.js
+++ helper/format.js
@@ -1,30 +1,30 @@
-import { ReasonPhrases, StatusCodes as SC } from 'http-status-codes'
-import { getBase64Signature } from '#helper/templating.js'
+import { ReasonPhrases, StatusCodes as SC } from 'http-status-codes';
+import { getBase64Signature } from '#helper/templating.js';
 
 const errorFormat = (e) => {
   return {
     stCode: e.statusCode || SC.INTERNAL_SERVER_ERROR,
-    message: e.message || ReasonPhrases.INTERNAL_SERVER_ERROR,
-  }
-}
+    message: e.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+  };
+};
 
 const excelFormat = (row) => {
-  let lt = row['Logika Berpikir']
-  let aa = row['Kemampuan Analisa']
-  let na = row['Kemampuan Numerik']
-  let am = row['Motivasi Prestasi']
-  let ad = row['Penyesuaian Diri']
-  let es = row['Stabilitas Emosi']
-  let ld = row['Kepemimpinan']
-  let cm = row['Komunikasi']
-  let wa = row['Kesediaan Kerja']
-  let ws = row['Kecepatan Kerja']
-  let wac = row['Ketelitian Kerja']
-  let wd = row['Ketahanan Kerja']
-  let sc = row['Konsep Diri']
-  let mo = row['Motivasi']
-  let as = row['Aspirasi']
-  let signature = getBase64Signature(row['TTD Psikolog'])
+  let lt = row['Logika Berpikir'];
+  let aa = row['Kemampuan Analisa'];
+  let na = row['Kemampuan Numerik'];
+  let am = row['Motivasi Prestasi'];
+  let ad = row['Penyesuaian Diri'];
+  let es = row['Stabilitas Emosi'];
+  let ld = row['Kepemimpinan'];
+  let cm = row['Komunikasi'];
+  let wa = row['Kesediaan Kerja'];
+  let ws = row['Kecepatan Kerja'];
+  let wac = row['Ketelitian Kerja'];
+  let wd = row['Ketahanan Kerja'];
+  let sc = row['Konsep Diri'];
+  let mo = row['Motivasi'];
+  let as = row['Aspirasi'];
+  let signature = getBase64Signature(row['TTD Psikolog']);
 
   let finalObj = {
     name: row['Nama Peserta'],
@@ -45,45 +45,45 @@ const excelFormat = (row) => {
     total_score: Number(row['Total Skor Akhir']).toFixed(2),
     recommendation: row['Decision'],
     signature,
-    date: 'Desember 2024',
-  }
+    date: 'Desember 2024'
+  };
 
-  if (lt !== 0) finalObj[`lt_${lt}`] = 'x'
-  if (aa !== 0) finalObj[`aa_${aa}`] = 'x'
-  if (na !== 0) finalObj[`na_${na}`] = 'x'
-  if (am !== 0) finalObj[`am_${am}`] = 'x'
-  if (ad !== 0) finalObj[`ad_${ad}`] = 'x'
-  if (es !== 0) finalObj[`es_${es}`] = 'x'
-  if (ld !== 0) finalObj[`ld_${ld}`] = 'x'
-  if (cm !== 0) finalObj[`cm_${cm}`] = 'x'
-  if (wa !== 0) finalObj[`wa_${wa}`] = 'x'
-  if (ws !== 0) finalObj[`ws_${ws}`] = 'x'
-  if (wac !== 0) finalObj[`wac_${wac}`] = 'x'
-  if (wd !== 0) finalObj[`wd_${wd}`] = 'x'
-  if (sc !== 0) finalObj[`sc_${sc}`] = 'x'
-  if (mo !== 0) finalObj[`mo_${mo}`] = 'x'
-  if (as !== 0) finalObj[`as_${as}`] = 'x'
+  if (lt !== 0) finalObj[`lt_${lt}`] = 'x';
+  if (aa !== 0) finalObj[`aa_${aa}`] = 'x';
+  if (na !== 0) finalObj[`na_${na}`] = 'x';
+  if (am !== 0) finalObj[`am_${am}`] = 'x';
+  if (ad !== 0) finalObj[`ad_${ad}`] = 'x';
+  if (es !== 0) finalObj[`es_${es}`] = 'x';
+  if (ld !== 0) finalObj[`ld_${ld}`] = 'x';
+  if (cm !== 0) finalObj[`cm_${cm}`] = 'x';
+  if (wa !== 0) finalObj[`wa_${wa}`] = 'x';
+  if (ws !== 0) finalObj[`ws_${ws}`] = 'x';
+  if (wac !== 0) finalObj[`wac_${wac}`] = 'x';
+  if (wd !== 0) finalObj[`wd_${wd}`] = 'x';
+  if (sc !== 0) finalObj[`sc_${sc}`] = 'x';
+  if (mo !== 0) finalObj[`mo_${mo}`] = 'x';
+  if (as !== 0) finalObj[`as_${as}`] = 'x';
 
-  return finalObj
-}
+  return finalObj;
+};
 
 const excelFormatPrestasi = (row) => {
-  let lt = row['Logika Berpikir']
-  let aa = row['Kemampuan Analisa']
-  let na = row['Kemampuan Numerik']
-  let am = row['Motivasi Prestasi']
-  let ad = row['Penyesuaian Diri']
-  let es = row['Stabilitas Emosi']
-  let ld = row['Kepemimpinan']
-  let cm = row['Komunikasi']
-  let wa = row['Kesediaan Kerja']
-  let ws = row['Kecepatan Kerja']
-  let wac = row['Ketelitian Kerja']
-  let wd = row['Ketahanan Kerja']
-  let sc = row['Konsep Diri']
-  let mo = row['Motivasi']
-  let as = row['Aspirasi']
-  let signature = getBase64Signature(row['TTD Psikolog'])
+  let lt = row['Logika Berpikir'];
+  let aa = row['Kemampuan Analisa'];
+  let na = row['Kemampuan Numerik'];
+  let am = row['Motivasi Prestasi'];
+  let ad = row['Penyesuaian Diri'];
+  let es = row['Stabilitas Emosi'];
+  let ld = row['Kepemimpinan'];
+  let cm = row['Komunikasi'];
+  let wa = row['Kesediaan Kerja'];
+  let ws = row['Kecepatan Kerja'];
+  let wac = row['Ketelitian Kerja'];
+  let wd = row['Ketahanan Kerja'];
+  let sc = row['Konsep Diri'];
+  let mo = row['Motivasi'];
+  let as = row['Aspirasi'];
+  let signature = getBase64Signature(row['TTD Psikolog']);
 
   let finalObj = {
     name: row['Nama Peserta'],
@@ -102,26 +102,94 @@ const excelFormatPrestasi = (row) => {
     total_score: Number(row['Total Skor Akhir']).toFixed(2),
     recommendation: row['Decision'],
     signature,
-    date: 'November 2024',
+    date: 'November 2024'
+  };
+
+  if (lt !== 0) finalObj[`lt_${lt}`] = 'x';
+  if (aa !== 0) finalObj[`aa_${aa}`] = 'x';
+  if (na !== 0) finalObj[`na_${na}`] = 'x';
+  if (am !== 0) finalObj[`am_${am}`] = 'x';
+  if (ad !== 0) finalObj[`ad_${ad}`] = 'x';
+  if (es !== 0) finalObj[`es_${es}`] = 'x';
+  if (ld !== 0) finalObj[`ld_${ld}`] = 'x';
+  if (cm !== 0) finalObj[`cm_${cm}`] = 'x';
+  if (wa !== 0) finalObj[`wa_${wa}`] = 'x';
+  if (ws !== 0) finalObj[`ws_${ws}`] = 'x';
+  if (wac !== 0) finalObj[`wac_${wac}`] = 'x';
+  if (wd !== 0) finalObj[`wd_${wd}`] = 'x';
+  if (sc !== 0) finalObj[`sc_${sc}`] = 'x';
+  if (mo !== 0) finalObj[`mo_${mo}`] = 'x';
+  if (as !== 0) finalObj[`as_${as}`] = 'x';
+
+  return finalObj;
+};
+
+const excelNewFormat = (row) => {
+  // Implementasi format baru jika diperlukan
+  let id = row['No'];
+  let name = row['Nama Peserta'];
+  let university = row['Universitas'];
+  let ro = row['RO'];
+  let iq_scale = row['IQ IST'];
+  let smart_grade = row['Kategori IQ'];
+  let v_logic = Number(row['Logika Berpikir']).toFixed(2);
+  let v_verbal = Number(row['Penalaran Verbal']).toFixed(2);
+  let v_nonverbal = Number(row['Penalaran Non Verbal']).toFixed(2);
+  let v_numeric = Number(row['Penalaran Numerik']).toFixed(2);
+  let v_analytic = Number(row['Kemampuan Analisa']).toFixed(2);
+  let v_consistency = Number(row['Keajegan Kerja']).toFixed(2);
+  let v_speed = Number(row['Kecepatan Kerja']).toFixed(2);
+  let v_accuracy = Number(row['Ketelitian Kerja']).toFixed(2);
+  let v_endurance = Number(row['Ketahanan Kerja']).toFixed(2);
+  let v_motivation = Number(row['Motivasi']).toFixed(2);
+  let v_social = Number(row['Kemampuan Sosial']).toFixed(2);
+  let v_leadership = Number(row['Kepemimpinan']).toFixed(2);
+  let v_maturity = Number(row['Kematangan']).toFixed(2);
+  let health_summary = row['Kesimpulan'];
+  let total_score = Number(row['Total Score Akhir']).toFixed(2);
+  let decision = row['Decision'];
+
+  let finalObj = {
+    id,
+    name,
+    campus: university,
+    regional: ro,
+    test_date: 'Oktober 2025',
+    iq_scale,
+    smart_grade,
+    v_logical: v_logic,
+    v_verbal,
+    v_nonverbal,
+    v_numeric,
+    v_analysis: v_analytic,
+    v_consistency,
+    v_speed,
+    v_accuracy,
+    v_endurance,
+    v_motivation,
+    v_social,
+    v_leadership,
+    v_maturity,
+    total_score,
+    recommendation_status: decision,
+    normal_status: health_summary
+  };
+
+  if (health_summary === 'Normal') {
+    finalObj.normal = 'v';
+    finalObj.mild = '';
+    finalObj.severe = '';
+  } else if (health_summary === 'Kecenderungan Gangguan') {
+    finalObj.normal = '';
+    finalObj.mild = 'v';
+    finalObj.severe = '';
+  } else if (health_summary === 'Gangguan') {
+    finalObj.normal = '';
+    finalObj.mild = '';
+    finalObj.severe = 'v';
   }
 
-  if (lt !== 0) finalObj[`lt_${lt}`] = 'x'
-  if (aa !== 0) finalObj[`aa_${aa}`] = 'x'
-  if (na !== 0) finalObj[`na_${na}`] = 'x'
-  if (am !== 0) finalObj[`am_${am}`] = 'x'
-  if (ad !== 0) finalObj[`ad_${ad}`] = 'x'
-  if (es !== 0) finalObj[`es_${es}`] = 'x'
-  if (ld !== 0) finalObj[`ld_${ld}`] = 'x'
-  if (cm !== 0) finalObj[`cm_${cm}`] = 'x'
-  if (wa !== 0) finalObj[`wa_${wa}`] = 'x'
-  if (ws !== 0) finalObj[`ws_${ws}`] = 'x'
-  if (wac !== 0) finalObj[`wac_${wac}`] = 'x'
-  if (wd !== 0) finalObj[`wd_${wd}`] = 'x'
-  if (sc !== 0) finalObj[`sc_${sc}`] = 'x'
-  if (mo !== 0) finalObj[`mo_${mo}`] = 'x'
-  if (as !== 0) finalObj[`as_${as}`] = 'x'
+  return finalObj;
+};
 
-  return finalObj
-}
-
-export { errorFormat, excelFormat, excelFormatPrestasi }
+export { errorFormat, excelFormat, excelFormatPrestasi, excelNewFormat };
